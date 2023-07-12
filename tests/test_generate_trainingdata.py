b80b380adbb14d2edbfd1fecd9511570e7b6b2af
kolla <eirik.matias@gmail.com>
2023-07-12 03:52:28 -0700

    save

--- tests/test_generate_trainingdata.py
+++ tests/test_generate_trainingdata.py
@@ -34,26 +34,33 @@ def test_grams1():
 
 def test_grams2():
 
-    text = "i was happy with the movie but not with the popcorn"
+    text = "i have seen this movie and i liked it very much"
     tokens = text.split()
 
     # make vocabulary with unigrams, bigrams and trigrams
     vocabulary = {
         "i" : 1,
-        "was happy" : 2,
-        "happy with" : 1,
-        "happy with the" : 1,
-        "the movie but" : 1,
-        "but" : 1,
-        "not with" : 1,
-        "with the popcorn" : 1,
+        "i have" : 2,
+        "have seen this" : 1,
+        "seen this movie" : 1,
+        "this movie" : 1,
+        "this movie and" : 1,
+        "i liked" : 1,
+        "liked it" : 1,
+        "it very" : 1,
+        "very much" : 1,
     }
 
     sentiment = 1
     threshold = 0.0
 
+    grams, weights  = generate_trainingdata.find_grams(tokens, vocabulary)
+    print(grams)
+    fit_grams, fit_weights = generate_trainingdata.fit_grams(grams, weights)
+    print(grams)
+
 
     
 if __name__ == '__main__':
     
-    pass
+    pass 
