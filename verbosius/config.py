880dc931427665a00b0b9dcf53b2cbf76f4659c7
TobiasBrambo <tobias.m.brambo@gmail.com>
2023-07-13 09:21:39 +0200

    changed params in config, added dynamic path for run.sh. TODO: Dyn paths in config.py?

--- verbosius/config.py
+++ verbosius/config.py
@@ -3,8 +3,8 @@ preproc_input_path="/home/tobxtra/data/verbosius/imdb/"
 preproc_output_path="/home/tobxtra/data/verbosius/store_imdb_pickle/"
 
 dataset="imdb"
-batch_size=12500
-batch_amount=2
+batch_size=25000
+batch_amount=1
 use_test_set=True
 shuffle=True
 seed=42
@@ -17,12 +17,12 @@ traindat_output_path="/home/tobxtra/data/verbosius/store_imdb_trainingdata/"
 batchdist_n=0
 
 # TM PARAMS
-MAX_DF=0.7
-MIN_DF=10
+MAX_DF=0.5263
+MIN_DF=21
 MAX_FEATURES=5000
 N_GRAM_RANGE=(1, 2)
-NUMBER_OF_CLAUSES=8000
-LITERAL_BUDGET=10
-S=42.69
-T=1000
-TM_EPOCHS=10
+NUMBER_OF_CLAUSES=5853
+LITERAL_BUDGET=8
+S=3.4606
+T=9225
+TM_EPOCHS=7
