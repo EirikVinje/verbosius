01337162e9426d556fffd6974b23f454f1c7b281
kolla <eirik.matias@gmail.com>
2023-08-21 10:27:31 +0200

    added make_env to quickly make directory for data handling

---
+++ make_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~
+mkdir -p data && cd data
+mkdir -p verbosius && cd verbosius
+mkdir -p imdb && cd imdb
+
+mkdir -p chunker && rm -rf chunker/*
+mkdir -p preprocess && rm -rf preprocess/*
+mkdir -p trainingdata && rm -rf trainingdata/*
+mkdir -p models
+
+
