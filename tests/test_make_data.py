7d40ae0319d7e7f3c46959e1f3911efcaadce631
Eirik Vinje <eirik.matias@gmail.com>
2024-03-21 14:42:51 +0100

    test for pipeline

---
+++ tests/test_make_data.py
@@ -0,0 +1,24 @@
+from chunking.chunker import Chunker
+from preprocess.preprocess import Preprocess
+from trainingdata.weighter import Weighter
+from train_eval_tokenize.trainingdata import Trainingdata
+import config
+
+def make_data():
+
+    config.root = "/home/bigtech/data/verbosius/testing/root" 
+    part_n = 1234
+
+    # Chunker(size="big", part_n=part_n, n_chunks=10, progress_bar=True, force_write=True).run()
+    # Preprocess(part_n=part_n, progress_bar=True, force_write=True).run()
+    # Weighter(part_n=part_n, progress_bar=True, force_write=True).run()
+    
+    trainingdata = Trainingdata(part_n=part_n, progress_bar=True, force_write=True)
+
+    print(trainingdata._get_class_balance())
+
+    trainingdata.run()
+
+
+if __name__ == "__main__":
+    make_data()
