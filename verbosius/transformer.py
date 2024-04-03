db4c876d577730e964b934e50f694fb09f990e82
Eirik Vinje <eirik.matias@gmail.com>
2024-04-03 09:35:34 +0200

    transformer now working

--- verbosius/transformer.py
+++ verbosius/transformer.py
@@ -54,81 +54,6 @@ class Transformer:
 
     def _set_model(self):
         self.model = CustomModel(config.num_tok_labels, config.num_seq_labels, config.neutral_weight, config.loss_weight).to(config.device)
-     
-
-    def _data_collator(self, batch_input):
-
-        input_ids = [torch.tensor(inst["input_ids"], dtype=torch.long) for inst in batch_input]
-        attention_mask = [torch.tensor(inst["attention_mask"], dtype=torch.long) for inst in batch_input]
-        targets = [torch.tensor(inst["targets"], dtype=torch.long) for inst in batch_input]
-        labels = [torch.tensor(inst["labels"], dtype=torch.long) for inst in batch_input] if "labels" in batch_input[0].keys() else None
-        sentiment = [torch.tensor(inst["sentiment"], dtype=torch.long) for inst in batch_input] if "sentiment" in batch_input[0].keys() else None
-
-        input_ids = pad_sequence(input_ids, batch_first=True, padding_value=1)
-        attention_mask = pad_sequence(attention_mask, batch_first=True, padding_value=0)
-        targets = pad_sequence(targets, batch_first=True, padding_value=0)
-        
-        if labels != None:
-            labels = pad_sequence(labels, batch_first=True, padding_value=-100) 
-
-        if sentiment == None and labels == None:
-            
-            new_batch_input = {
-            "input_ids": input_ids.to(config.device),
-            "attention_mask": attention_mask.to(config.device),
-            "targets": targets.to(config.device)
-            }
-
-            return new_batch_input
-
-        new_batch_input = {
-            "input_ids": input_ids.to(config.device),
-            "attention_mask": attention_mask.to(config.device),
-            "labels": labels.to(config.device),
-            "targets": targets.to(config.device),
-            "sentiment": torch.stack(sentiment).to(config.device)
-        }
-
-        return new_batch_input
-
-
-    def _compute_metrics(self, eval_preds):
-    
-        metric = evaluate.load("accuracy")
-        logits, labels = eval_preds
-        predictions = np.argmax(logits[1], axis=1)
-        token_predictions = np.argmax(logits[0], axis=2)
-        
-        nz = (predictions!=0).sum()
-        token_negative = (token_predictions==1).sum()
-        token_neutral = (token_predictions==0).sum()
-        token_positive = (token_predictions==2).sum()
-
-        seqeval = evaluate.load("seqeval")
-        
-        label_list = ["neutral", "negative", "positive"]
-
-        true_predictions = [
-            [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
-            for prediction, label in zip(token_predictions, labels[0])
-        ]
-
-        true_labels = [
-            [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
-            for prediction, label in zip(token_predictions, labels[0])
-        ]
-
-        results = seqeval.compute(predictions=true_predictions, references=true_labels)
-        output = {
-            "token_accuracy": results["overall_accuracy"],
-            "sequence_accuracy": (metric.compute(predictions=predictions, references=labels[1]))['accuracy'],
-            "nz": nz,
-            "token_neutral": token_neutral,
-            "token_negative": token_negative,
-            "token_positive": token_positive
-        }
-        
-        return output
 
 
     def _set_trainer(self):
@@ -141,9 +66,11 @@ class Transformer:
             
             output_dir = os.path.join(self.model_dir, self.model_name),
             num_train_epochs = config.num_train_epochs,
-            evaluation_strategy = "epoch",
-            save_strategy = "epoch",
-            load_best_model_at_end = True
+            evaluation_strategy = config.evaluation_strategy,
+            save_strategy = config.save_strategy,
+            load_best_model_at_end = True,
+            label_names=config.label_names
+            
             )
 
         if config.device != "cpu":
@@ -156,7 +83,7 @@ class Transformer:
             eval_dataset=self.eval_dataset,
             tokenizer=config.tokenizer,
             compute_metrics=self._compute_metrics,
-            data_collator=self._data_collator,
+            data_collator=self._custom_data_collator
             # optimizers=(AdamW(self.model.parameters(), lr=config.learning_rate), None)
             )
 
@@ -190,6 +117,88 @@ class Transformer:
         self._set_model()
         self._set_trainer()
         self._train()
+
+
+    def _custom_data_collator(self, batch_input):
+
+        input_ids = [torch.tensor(inst["input_ids"], dtype=torch.long) for inst in batch_input]
+        attention_mask = [torch.tensor(inst["attention_mask"], dtype=torch.long) for inst in batch_input]
+        targets = [torch.tensor(inst["targets"], dtype=torch.long) for inst in batch_input]
+        labels = [torch.tensor(inst["labels"], dtype=torch.long) for inst in batch_input] if "labels" in batch_input[0].keys() else None
+        sentiment = [torch.tensor(inst["sentiment"], dtype=torch.long) for inst in batch_input] if "sentiment" in batch_input[0].keys() else None
+
+        input_ids = pad_sequence(input_ids, batch_first=True, padding_value=1)
+        attention_mask = pad_sequence(attention_mask, batch_first=True, padding_value=0)
+        targets = pad_sequence(targets, batch_first=True, padding_value=0)
+        
+        if labels != None:
+            labels = pad_sequence(labels, batch_first=True, padding_value=-100) 
+
+        if sentiment == None and labels == None:
+            
+            new_batch_input = {
+            "input_ids": input_ids.to(config.device),
+            "attention_mask": attention_mask.to(config.device),
+            "targets": targets.to(config.device)
+            }
+
+            return new_batch_input
+
+        new_batch_input = {
+            "input_ids": input_ids.to(config.device),
+            "attention_mask": attention_mask.to(config.device),
+            "labels": labels.to(config.device),
+            "targets": targets.to(config.device),
+            "sentiment": torch.stack(sentiment).to(config.device)
+        }
+
+        return new_batch_input
+    
+
+    def _compute_metrics(self, eval_preds):
+    
+        metric = evaluate.load("accuracy")
+        logits, labels = eval_preds
+        predictions = np.argmax(logits[1], axis=1)
+        token_predictions = np.argmax(logits[0], axis=2)
+        
+        nz = (predictions!=0).sum()
+        token_negative = (token_predictions==1).sum()
+        token_neutral = (token_predictions==0).sum()
+        token_positive = (token_predictions==2).sum()
+
+        seqeval = evaluate.load("seqeval")
+        
+        label_list = ["neutral", "negative", "positive"]
+
+        true_predictions = [
+
+            [label_list[p] for (p, l) in zip(prediction, label) if l != -100]
+
+            for prediction, label in zip(token_predictions, labels[0])
+
+        ]
+
+        true_labels = [
+
+            [label_list[l] for (p, l) in zip(prediction, label) if l != -100]
+
+            for prediction, label in zip(token_predictions, labels[0])
+
+        ]
+
+        results = seqeval.compute(predictions=true_predictions, references=true_labels)
+        output = {
+            "token_accuracy": results["overall_accuracy"],
+            "sequence_accuracy": (metric.compute(predictions=predictions, references=labels[1]))['accuracy'],
+            "nz": nz,
+            "token_neutral": token_neutral,
+            "token_negative": token_negative,
+            "token_positive": token_positive
+        }
+        
+        return output
+
 
 
 class IterableDataset_Custom(torch.utils.data.IterableDataset):
@@ -229,6 +238,7 @@ class IterableDataset_Custom(torch.utils.data.IterableDataset):
 
 
 class CustomModel(nn.Module): # transformers.modeling_utils.PreTrainedModel
+
     def __init__(self, num_tok_labels, num_seq_labels, neutral_weight, loss_weight, model_name='distilroberta-base'): 
         super(CustomModel,self).__init__() 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -244,7 +254,6 @@ class CustomModel(nn.Module): # transformers.modeling_utils.PreTrainedModel
         self.cel = nn.CrossEntropyLoss(weight=torch.tensor([neutral_weight, 1.0, 1.0]).to(self.device))
         self.seq_cel = nn.CrossEntropyLoss()
         self.temp_evidence = None
-
 
     def forward(self, input_ids=None, attention_mask=None, labels=None, targets=None, sentiment=None):
         
@@ -279,7 +288,12 @@ class CustomModel(nn.Module): # transformers.modeling_utils.PreTrainedModel
         
         else:
             return TokenClassifierOutput(logits=logits)
-        
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
